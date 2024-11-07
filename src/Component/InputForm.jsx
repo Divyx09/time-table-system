147db720b6bc8c394a99670df72562e7d931e55e
Prakhar2818 <prakharattarde95@gmail.com>
2024-11-07 13:02:30 +0530

    useState

--- src/Component/InputForm.jsx
+++ src/Component/InputForm.jsx
@@ -2,34 +2,50 @@ import { InputNumber } from "antd";
 import React, { useEffect, useState } from "react";
 
 const InputForm = () => {
-  const [facultyDetails, setFacultyDetails] = useState({
-    facultyId: 0,
-    numberOfFaculty: 0,
-    facultyName: "",
-    assignedSubject: "",
-  });
-  const [subjectDetails, setSubjectDetails] = useState([
-    {
-      subjectId: 0,
-      subjectName: "",
-      assignedfaculty: "",
-      facultyId: 0,
-    },
-  ]);
-  const [classRoomDetails, setClassRoomDetails] = useState([
-    {
-      classRoomId: 0,
-      classRoomNumber: 0,
-      classCapcity: 0,
-    },
-  ]);
-  const [labDetails, setLabDetails] = useState([
-    {
-      labId: 0,
-      labNumber: 0,
-      labCapacity: 0,
-    },
-  ]);
+  // const [facultyDetails, setFacultyDetails] = useState({
+  //   facultyId: 0,
+  //   numberOfFaculty: 0,
+  //   facultyName: "",
+  //   assignedSubject: "",
+  // });
+  // const [subjectDetails, setSubjectDetails] = useState([
+  //   {
+  //     subjectId: 0,
+  //     subjectName: "",
+  //     assignedfaculty: "",
+  //     facultyId: 0,
+  //   },
+  // ]);
+  // const [classRoomDetails, setClassRoomDetails] = useState([
+  //   {
+  //     classRoomId: 0,
+  //     classRoomNumber: 0,
+  //     classCapcity: 0,
+  //   },
+  // ]);
+  // const [labDetails, setLabDetails] = useState([
+  //   {
+  //     labId: 0,
+  //     labNumber: 0,
+  //     labCapacity: 0,
+  //   },
+  // ]);
+  const [facultyId, setFacultyId] = useState("")
+  const [numberOfFaculty, setNumberOfFaculty] = useState("")
+  const [facultyName, setFacultyName] = useState("")
+  const [assignedSubject, setAssignedSubjects] = useState("")
+
+  const [subjectId, setSubjectId] = useState("")
+  const [subjectName, setSubjectName] = useState("")
+  const [assignedfaculty , setAssignedfaculty] = useState("")
+  
+  const [classRoomId, setClassRoomId] = useState("")
+  const [classRoomNumber, setClassRoomNumber] = useState("")
+  const [classCapacity, setClassCapacity] = useState("")
+
+  const [labId, setLabId] = useState("")
+  const [labNumber, setLabNumber] = useState("")
+  const [labCapacity, setLabCapacity] = useState("")
 
   const idGenrator = (initials) => {
     let id = "";
@@ -43,7 +59,7 @@ const InputForm = () => {
   }, [facultyDetails]);
 
   return (
-    <div className='row my-4'>
+    <div className='row my-4 mx-0'>
       <div className='col-6 mx-5'>
         <label>Number Of Faculty:</label>
         <InputNumber
